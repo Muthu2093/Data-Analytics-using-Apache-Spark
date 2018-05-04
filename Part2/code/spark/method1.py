0c8f4eb224b84a8a642e7b510886ce92de17802b
akshay993 <achopra6@buffalo.edu>
2018-05-03 23:47:28 -0400

    started

---
+++ Part2/code/spark/method1.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+def read_directory():
+    
+    
+
+
+
+def read_file():
+
+    
+
+
+
+	
