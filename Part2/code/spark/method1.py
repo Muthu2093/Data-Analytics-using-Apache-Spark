5b7ff0e41a9975fc9b3f4deb051848201a15276f
akshay993 <achopra6@buffalo.edu>
2018-05-05 13:27:01 -0400

    feature extraction partially done.. utf problem resolved

--- Part2/code/spark/method1.py
+++ Part2/code/spark/method1.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 
 import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 from operator import add
 
 #import pyspark
@@ -8,32 +10,31 @@ from operator import add
 from pyspark.sql import SparkSession
 from pyspark import SparkConf, SparkContext
 
-stop_words=["his","have","by","had","from","not","at","as","The","with","of","a", "an", "the", "this", "that","those", "on", "in", "for","he", "she", "they", "we", "I", "you", "who", "which", "where", "when", "why", "was", "were","while","under","my","me", "is", "hi" , "hey", "hello","to","and"]
+stop_words=["percent","than","can","her","about","would","said","more","has","or","i","are","will","but","it","be","at","said","his","have","by","had","from","not","at","as","The","with","of","a", "an", "the", "this", "that","those", "on", "in", "for","he", "she","their", "they", "we", "I", "you", "who", "which", "where", "when", "why", "was", "were","while","under","my","me", "is", "hi" , "hey", "hello","to","and"]
 
 
 
 def read_directory(sc, path):
     icount=0;
+    
     feature_list=[]
     textRDD=sc.textFile(path)
     words = textRDD.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1))
     wordcount = words.reduceByKey(add).map(lambda (x,y): (y,x)).sortByKey(ascending=False).collect()
-    for wc in wordcount:
-        print(wc)
-#    for (count, word) in wordcount:
-#        if word in stop_words:
-#            print("%s: %i" % (word, count))
-#            pass
-#        else:
-#            #print("%s: %i" % (word, count))
-#            if(icount!=10):
-#                feature_list.append(word)
-#                icount=icount+1
-#            else:
-#                break
-#            #print("%s: %i" % (word, count))
-    #print(textRDD)
-                
+    
+    
+    for (count, word) in wordcount:
+                    
+        if word.lower() in stop_words:
+            pass
+        else:
+            print("%s: %i" % (word, count))
+            if(icount!=10):
+                feature_list.append(word)
+                icount=icount+1
+            else:
+                break            
+                       
     return feature_list
 
 if __name__ == "__main__":
@@ -41,13 +42,13 @@ if __name__ == "__main__":
     conf = SparkConf().setAppName("Lab3")
     conf=conf.setMaster("local[*]")
     sc=SparkContext(conf=conf)
-    path="/home/hadoop/spark/data_lab3/data/Sports/*.txt"
+    path="/home/hadoop/spark/data_lab3/data/Business/*.txt"
     
     
     bs_list= read_directory(sc,path)
     
-    #for word in bs_list:
-    #    print("%s: " % (word))
+    for word in bs_list:
+        print("%s: " % (word))
 
 
 
